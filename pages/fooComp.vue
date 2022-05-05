e37b28abc744fa32d4395a2ed0e414d104bad942
liuhui <liuhui@cnksi.com>
2022-05-05 14:37:01 +0800

    init project

---
+++ pages/fooComp.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Date: 2022-05-05 14:29:12
+ * @Author: liuhui
+ * @LastEditTime: 2022-05-05 14:29:57
+ * @LastEditors: liuhui
+ * @Description: foo 页面
+-->
+
+<template>
+  <div class="foo">
+    <h2>hello foo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Date: 2022-05-05 14:21:19
+ * @Author: liuhui
+ * @LastEditTime: 2022-05-05 14:28:48
+ * @LastEditors: liuhui
+ * @Description: 
+-->
+<template>
+  <!-- <Tutorial /> -->
+  <div class="hello">
+    <h2>hello nuxt</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
